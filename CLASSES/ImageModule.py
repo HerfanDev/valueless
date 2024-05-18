73929981af7763acd214cbbf12d4bc99e4388899
erfan hajian <myphp@gmail.com>
2024-05-18 03:06:44 -0700

    کلاس ImageModule کامل شد

--- CLASSES/ImageModule.py
+++ CLASSES/ImageModule.py
@@ -1,5 +1,30 @@
 import cv2
 class Image:
     def __init__(self, src : str):
-        self.src:str = src
-        self.__h,self.__w,self.__c=cv2.imread(self.src).shape
+        self.__src:str = src
+        self.__h,self.__w,self.__c=cv2.imread(self.__src).shape
+    
+    @property
+    def HeightPX(self):
+        return self.__h
+    
+    @property
+    def WidthPX(self):
+        return self.__w
+    
+    @property
+    def ChannelsCount(self):
+        return self.__c
+    @property
+    def Src(self):
+        return self.__src
+    
+    @Src.setter
+    def Src(self,src:str):
+        self.__src = src
+        self.__h,self.__w,self.__c=cv2.imread(self.__src).shape
+    
+    
+
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-path = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\1.jpg'
+path1 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\1.jpg'
+path2 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\2.jpg'
 from CLASSES.ImageModule import Image
 
-objImage = Image(path)
+objImage = Image(path1)
+print(objImage.Src)
+print('c : ',objImage.ChannelsCount)
+print('w : ',objImage.WidthPX)
+print('h : ',objImage.HeightPX)
+objImage.Src = path2
+print(objImage.Src)
+print('c : ',objImage.ChannelsCount)
+print('w : ',objImage.WidthPX)
+print('h : ',objImage.HeightPX)
