1a36e9106e60215f5725a47d2108108c9dc86119
erfan hajian <myphp@gmail.com>
2024-05-18 05:36:55 -0700

    استفاده کامپوزیشن کلاس کانواس از ایمیج و متد ریدرا کانواس اضافه شد

---
+++ CLASSES/CanvasModule.py
@@ -0,0 +1,21 @@
+from .ImageModule import Image
+import cv2
+
+class Canvas:
+    def __init__(self,imagePath = str):
+        self.__ObjImage = Image(imagePath)
+        self.redraw()
+
+    @property
+    def Picture(self):
+        return self.__ObjImage
+    
+    @Picture.setter
+    def Picture(self,imgObject:Image):
+        self.__ObjImage = imgObject;
+
+    def redraw(self):
+        matlike = cv2.imread(self.Picture.Src)
+        cv2.imshow('valueless windows',matlike)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-path1 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\1.jpg'
-path2 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\2.jpg'
+from CLASSES.CanvasModule import Canvas
 from CLASSES.ImageModule import Image
 
-objImage = Image(path1)
-print(objImage.Src)
-print('c : ',objImage.ChannelsCount)
-print('w : ',objImage.WidthPX)
-print('h : ',objImage.HeightPX)
-objImage.Src = path2
-print(objImage.Src)
-print('c : ',objImage.ChannelsCount)
-print('w : ',objImage.WidthPX)
-print('h : ',objImage.HeightPX)
+strPath1 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\1.jpg'
+strPath2 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\2.jpg'
+strPath3 = r'C:\Users\BURUJ\Desktop\github\prj2\valueless\IMAGES\3.jpg'
+
+def GetInfoOfCanvasImage(objCanvas : Canvas):
+    print('\tcanvas image source :',objCanvas.Picture.Src)
+    print('\tcanvas image width  :',objCanvas.Picture.WidthPX)
+    print('\tcanvas image height :',objCanvas.Picture.HeightPX)
+    print('\tcanvas image channels count :',objCanvas.Picture.ChannelsCount)
+
+objCanvas = Canvas(strPath1)    #create a object from Canvas class
+objCanvas.Picture.Src = strPath2
